71b342a52adc83fb4c752c3580f22a6903f42dd1
marco <gamesmrco@gmail.com>
2021-07-17 01:21:24 -0300

    falta add AH

--- script.js
+++ script.js
@@ -1,15 +1,85 @@
 const getBtn = document.getElementById('get-btn');
 
-let product = [['ENCHANTED_RAW_SALMON', 0], ['ENCHANTED_ACACIA_LOG', 0], ['ENCHANTED_BLAZE_POWDER', 0], ['ENCHANTED_BLAZE_ROD', 0], ['ENCHANTED_COAL_BLOCK', 0], ['ENCHANTED_BONE', 0]];
+window.onload = () => {
+  getData();
+}
 
-//OBJETOS (PETS)
+let sortDirection = false;
 
-const BabyYeti = {
-  name: "Baby Yeti",
-  dias: 12,
-  item: "E. Raw Salmon",
-  qntd: 16,
-  valor: product[0][1],
+//PRODUTOS
+
+let product = [
+              ['ENCHANTED_RAW_SALMON', 0, "Baby Yeti", 12, "E. Raw Salmon", 16, 20000000], 
+              ['ENCHANTED_RED_MUSHROOM', 0, "Bat", 3, "E. Red Mushroom", 64, 250000],
+              ['ENCHANTED_GOLD_BLOCK', 0, "Bee", 3, "E. Gold Block", 9, 450000],
+            ];
+
+//PETS
+
+let pets = [];
+
+function carregaTabela(pets) {
+  const tableBody = document.getElementById('tableData');
+  let dataHtml = '';
+  let i = 1;
+  for(let pet of pets) {
+    dataHtml += `<tr>
+                  <td>${i}</td>
+                  <td>${pet.nome}</td>
+                  <td>${pet.dias}</td>
+                  <td>${pet.item}</td>
+                  <td>${pet.qntd}</td>
+                  <td>${pet.valor}</td>
+                  <td>${pet.total().toLocaleString('pt-BR')}</td>
+                  <td>${pet.upgrade}</td>
+                  <td>${pet.epicb}</td>
+                  <td>${pet.legb}</td>
+                  <td>${pet.profit().toLocaleString('pt-BR')}</td>
+                  <td>${pet.pdia().toLocaleString('pt-BR')}</td>
+                <tr/>`
+    i += 1;
+  }
+
+  tableBody.innerHTML = dataHtml;
+}
+
+const getData = () => {
+  fetch('https://api.slothpixel.me/api/skyblock/bazaar/').then(response => {
+    return response.json();
+  })
+  .then(responseData => {
+    for (var i = 0; i < product.length; i++) {
+      product[i][1] = Math.round(responseData[product[i][0]].quick_status.buyPrice*100)/100
+      pets[i] = {
+        valor: product[i][1],
+        nome: product[i][2],
+        dias: product[i][3],
+        item: product[i][4],
+        qntd: product[i][5],
+        upgrade: product[i][6],
+        epicb: 200000000,
+        legb: 200000000,
+        total: function(){
+          return Math.round(this.qntd*this.valor);
+        },
+        profit: function(){
+          return Math.round(this.legb-(this.epicb+this.upgrade+this.total()))
+        },
+        pdia: function(){
+          return Math.round(this.profit()/this.dias)
+        }
+      }
+    }
+  });
+};
+
+
+getBtn.addEventListener('click', function() {
+  carregaTabela(pets);
+});
+
+/*
+valor: product[0][1],
   total: function() {
     return this.qntd*this.valor;
   },
@@ -21,27 +91,5 @@ const BabyYeti = {
   },
   pdia: function() {
     return this.profit/this.dias;
-  },
-}
-
-
-
-
-const getData = () => {
-  fetch('https://api.slothpixel.me/api/skyblock/bazaar/').then(response => {
-    return response.json();
-  })
-  .then(responseData => {
-    for (var i = 0; i < product.length; i++) {
-      if (product[i][0] == "ENCHANTED_ACACIA_LOG") {
-        console.log('index: '+i);
-      }
-    }
-    if (BabyYeti.profit() > 1) {
-      
-    }
-  });
-};
-
-
-getBtn.addEventListener('click', getData);
+  }
+*/
